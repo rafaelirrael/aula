21b7903e6c2a7ee233012ff9d6f826fb02a0de90
rafaelirrael <151583985+rafaelirrael@users.noreply.github.com>
2024-04-29 20:19:44 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+$('.novidades button').click(function(){
+    $('.novidades').addClass("painel-aberto");
+})
+console.log($('.novidades button'))
+
+$('.mais-vendidos button').click(function(){
+    $('.mais-vendidos').addClass("painel-aberto");
+})
+
